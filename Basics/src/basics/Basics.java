ea33737d65906e3c3affa0ca0acfdab9ae31ba85
Bruno Vicente <brunovicente.dev@gmail.com>
2023-11-16 15:04:16 -0300

    First commit

---
+++ Basics/src/basics/Basics.java
@@ -0,0 +1,8 @@
+package basics;
+
+public class Basics {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Let's learn some Java");
+    }
+}
