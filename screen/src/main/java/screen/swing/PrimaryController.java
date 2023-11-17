618f214b029b00d72072963f2e201279edf53e37
Bruno Vicente <brunovicente.dev@gmail.com>
2023-11-17 14:53:31 -0300

    Commit switch screens

--- screen/src/main/java/screen/swing/PrimaryController.java
+++ screen/src/main/java/screen/swing/PrimaryController.java
@@ -2,11 +2,22 @@ package screen.swing;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class PrimaryController {
 
     @FXML
+    private Label labelMsg ;
+    private Button clickMe ;
+
+    @FXML
+    private void showHello() throws IOException {
+        labelMsg.setText("Hello World");
+    }
+
+    @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+       App.setRoot("secondary");
     }
 }
