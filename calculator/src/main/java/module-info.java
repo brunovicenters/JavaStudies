d918b49bdcc59fe3f86683859b28e4d84be96e87
Bruno Vicente <brunovicente.dev@gmail.com>
2023-11-18 15:47:26 -0300

    Back to basics

--- calculator/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module calculator.app.calculator {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens calculator.app.calculator to javafx.fxml;
-    exports calculator.app.calculator;
-}
--- screen/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module screen.swing {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens screen.swing to javafx.fxml;
-    exports screen.swing;
-}
