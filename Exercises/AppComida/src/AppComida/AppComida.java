8bb2c94246cbd7a6f7a8dd1ea512f7ae06907628
bruno <brunovicente.dev@gmail.com>
2024-02-29 19:26:46 -0300

    Added App Comida

--- Exercises/AppComida/src/AppComida/AppComida.java
+++ Exercises/AppComida/src/AppComida/AppComida.java
@@ -1,4 +1,32 @@
 package AppComida;
 
 public class AppComida {
+    public int codigoPedido;
+    public String comida;
+    public int quantidade;
+    public float precoFinal;
+    public int status;
+    public String restaurante;
+
+    public void getStatus(){
+        java.lang.String situacao = "";
+        if (status == 0) {
+            situacao = "em Análise";
+        } else if (status == 1) {
+            situacao = "em Preparo";
+        } else if (status == 2) {
+            situacao = "a Caminho";
+        } else if (status == 3) {
+            situacao = "Entregue";
+        }
+        System.out.println("O pedido " + codigoPedido + " está " + situacao);
+    }
+
+    public void addValor(float pedido){
+        precoFinal += pedido;
+    }
+
+    public String getRestaurante() {
+        return restaurante;
+    }
 }
--- Exercises/AppComida/src/Main.java
+++ Exercises/AppComida/src/Main.java
@@ -1,5 +1,38 @@
+import AppComida.AppComida;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        AppComida pedido1 = new AppComida();
+        pedido1.codigoPedido = 1;
+        pedido1.comida = "Almondega";
+        pedido1.quantidade = 3;
+        pedido1.precoFinal = 13.5F;
+        pedido1.status = 1;
+        pedido1.restaurante = "Seu Almon";
+
+        pedido1.getStatus();
+        System.out.println(pedido1.getRestaurante());
+
+        AppComida pedido2 = new AppComida();
+        pedido2.codigoPedido = 2;
+        pedido2.comida = "Coxinhas";
+        pedido2.quantidade = 7;
+        pedido2.precoFinal = 25.99F;
+        pedido2.status = 2;
+        pedido2.restaurante = "Seu Coxinha";
+
+        pedido2.getStatus();
+        System.out.println(pedido2.getRestaurante());
+
+        AppComida pedido3 = new AppComida();
+        pedido3.codigoPedido = 3;
+        pedido3.comida = "Kebabi";
+        pedido3.quantidade = 2;
+        pedido3.precoFinal = 56.55F;
+        pedido3.status = 0;
+        pedido3.restaurante = "Al Comida";
+
+        pedido3.getStatus();
+        System.out.println(pedido3.getRestaurante());
     }
 }
