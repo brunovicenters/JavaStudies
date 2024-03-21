818464bebae204184209857cae535157b8aebf75
bruno <brunovicente.dev@gmail.com>
2024-03-21 20:14:15 -0300

    Added final variables

---
+++ Exercises/Constant/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        final int idade_minima = 18;
+        System.out.println("Idade mínima é: " + idade_minima);
+
+        final String nome_empresa = "MinhaEmpresa";
+        System.out.println(nome_empresa);
+
+        final int x = 3;
+        final int y = 4;
+        final int z = 5;
+
+        System.out.println("X é: " + y);
+        System.out.println("Y é: " + z);
+        System.out.println("Z é: " + x);
+        System.out.println("A média é " + (x+y+z)/3);
+
+        final boolean estado_ativo = true;
+        System.out.println("O estado ativo é: " + estado_ativo);
+
+        final double pi = 3.14;
+        final int raio = 5;
+        final double area_circulo = pi*raio*raio;
+        System.out.println("A área do círculo é: " + area_circulo);
+    }
+}
